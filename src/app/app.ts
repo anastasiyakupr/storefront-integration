a6a97172ddb7fa43d6dc6f1f0b933757d16b8913
Anastasiia Antunovic <anastasiia.antunovic@dhimahi.com>
2026-08-07 13:35:40 +0200

    Integrate Promotershop drop-in components with staging BFF

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,29 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
+import '@promotershop/storefront-components';
+import { initStorefront } from '@promotershop/storefront-runtime';
+import { createStorefrontClient } from '@promotershop/storefront-sdk';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('storefront-integration');
+export class App implements OnInit {
+  async ngOnInit() {
+    const client = createStorefrontClient({
+      baseUrl: '',
+      affiliateId: 'LWF',
+    });
+
+    const app = await initStorefront({
+      root: document.getElementById('shop')!,
+      client,
+    });
+
+    await app.renderProduct('2000145825');
+
+    const { cart } = app.store.getSnapshot();
+    console.log("totalCount=", cart.totalCount); // number of items
+  }
 }
